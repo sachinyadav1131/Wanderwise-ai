6b837a40d8af248feca5b97d7168bc2cce94d354
Sachin Kumar <sachinyadav8940@gmail.com>
2026-08-07 01:37:42 +0530

    fix: Enable secure and sameSite cookie options for cross-origin production session support

--- Backend/utils/apiResponse.js
+++ Backend/utils/apiResponse.js
@@ -6,6 +6,8 @@ export const sendToken = (user, statusCode, message, res) => {
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
+        secure: true,
+        sameSite: "none",
     };
 
     res.status(statusCode).cookie("token", token, options).json({
